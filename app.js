0d66b02b500a5be7bfacfeab2d8d03bbb2af3b9e
Kris Shinn <raggamuffin.music@gmail.com>
2018-12-21 22:26:45 -0800

    Re-organized project. Some clean-up (#3)

--- app.js
+++ app.js
@@ -3,45 +3,17 @@ const os = require('os')
 const Buffer = require('safe-buffer').Buffer
 const { randomBytes } = require('crypto')
 const devp2p = require('ethereumjs-devp2p')
-const mongoose = require('mongoose')
 const geoip = require('geoip-lite')
+const debug = require('debug')('infura:dpt')
 const Web3 = require('web3')
-
 const web3 = new Web3()
+const _ = require('lodash')
+const db = require('./lib/db')()
+const web3Url = `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`
+web3.setProvider(new web3.providers.HttpProvider(web3Url))
 
-// Web3 Connection
-const WEB3_URL = 'https://mainnet.infura.io/v3/09bcdf3656ee4a80b05a4a921358d109'
-web3.setProvider(new web3.providers.HttpProvider(WEB3_URL))
-
-// MongoDB Connection
-const MONGO_URL = 'mongodb://localhost:27017'
-var db = mongoose.connection
-
-var Schema = mongoose.Schema
-const peerSchema = new Schema({
-  enode: { type: String, index: true, unique: true },
-  address: { type: String, index: true },
-  latitude: Number,
-  longitude: Number,
-  country: String,
-  port: Number,
-  clientId: { type: String, index: true },
-  clientMeta1: { type: String, index: true },
-  clientMeta2: { type: String, index: true },
-  clientMeta3: { type: String, index: true },
-  clientMeta4: { type: String, index: true },
-  capabilities: Array,
-  bestHash: String,
-  totalDifficulty: String,
-  bestBlockNumber: { type: Number, index: true, default: 0 },
-  infuraBlockNumber: { type: Number, index: true, default: 0 },
-  infuraDrift: { type: Number, index: true, default: 0 },
-  numPeers: { type: Number, default: 0 },
-  timestamp: { type: Date, index: true }
-})
-
-var EthPeer = mongoose.model('Peer', peerSchema)
-
+const EthPeer = db.EthPeer
+const conn = db.getConnection()
 const PRIVATE_KEY = randomBytes(32)
 const BOOTNODES = require('ethereum-common').bootstrapNodes.map((node) => {
   return {
@@ -84,62 +56,60 @@ const rlpx = new devp2p.RLPx(PRIVATE_KEY, {
 rlpx.on('error', (err) => console.error(chalk.red(`RLPx error: ${err.stack || err}`)))
 
 rlpx.on('peer:added', (peer) => {
-  var hello = peer.getHelloMessage()
-  var b = new EthPeer()
-  b.enode = hello.id.toString('hex')
-  b.address = peer._socket.remoteAddress
-  b.port = peer._socket.remotePort
-  var peerGeo = geoip.lookup(b.address)
-  if ((peerGeo) && (peerGeo.ll) && (peerGeo.ll.country)) {
-    b.country = peerGeo.ll.country
-  }
-  if ((peerGeo) && (peerGeo.city)) {
-    b.city = peerGeo.city
-  }
-  b.latitude = peerGeo.ll[0]
-  b.longitude = peerGeo.ll[1]
-  b.clientId = hello.clientId
-  var splitClientId = hello.clientId.split('/')
-  b.clientMeta1 = splitClientId[0]
-  b.clientMeta2 = splitClientId[1]
-  b.clientMeta3 = splitClientId[2]
-  b.clientMeta4 = splitClientId[3]
-  b.capabilities = hello.capabilities
-  b.timestamp = new Date()
-  const eth = peer.getProtocols()[0]
-  eth.sendStatus(myStatus)
-  eth.once('status', (peerStatus) => {
-    // console.log('received STATUS')
-    b.bestHash = '0x' + peerStatus.bestHash.toString('hex')
-    b.totalDifficulty = peerStatus.td.toString('hex')
-    web3.eth.getBlock(b.bestHash, false)
-      .then(function (block) {
-        if ((block) && (block.number)) {
-          b.bestBlockNumber = block.number
-        } else {
-          web3.eth.getBlockNumber()
-            .then(function (infuraBlockNumber) {
-              b.infuraBlockNumber = infuraBlockNumber
-              b.infuraDrift = Math.abs(b.infuraBlockNumber - b.bestBlockNumber)
-              mongoose.connect(MONGO_URL)
-              db.on('error', console.error.bind(console, 'connection error:'))
-              db.once('open', function () {
-                b.save()
-                console.log('Saved peer: ' + b.enode)
-              })
-                .catch(function (err) {
-                  console.error(err)
-                })
+    let hello = peer.getHelloMessage()
+    let peerGeo = geoip.lookup(peer._socket.remoteAddress)
+    let b = new EthPeer({
+      address: peer._socket.remoteAddress,
+      capabilitites: hello.capabilities,
+      clientId: hello.clientId,
+      enode: hello.id.toString('hex'),
+      port: peer._socket.remotePort,
+      timestamp: new Date(),
+    })
+
+    if (_.has(peerGeo, 'll.country')) {
+        b.country = peerGeo.ll.country
+    }
+    if (_.has(peerGeo, 'city')) {
+        b.city = peerGeo.city
+    }
+    b.latitude = peerGeo.ll[0]
+    b.longitude = peerGeo.ll[1]
+
+    var splitClientId = hello.clientId.split('/')
+    b.clientMeta1 = splitClientId[0]
+    b.clientMeta2 = splitClientId[1]
+    b.clientMeta3 = splitClientId[2]
+    b.clientMeta4 = splitClientId[3]
+
+    const eth = peer.getProtocols()[0]
+    eth.sendStatus(myStatus)
+    eth.once('status', (peerStatus) => {
+        debug(`Received status ${peer._socket.remoteAddress}`)
+        b.bestHash = '0x' + peerStatus.bestHash.toString('hex')
+        b.totalDifficulty = peerStatus.td.toString('hex')
+        web3.eth.getBlock(b.bestHash, false)
+        .then((block) => {
+          if (_.has(block, 'number')) {
+            debug(`Received: ${block.number}`)
+            b.bestBlockNumber = block.number
+          }
+          return web3.eth.getBlockNumber()
+        })
+        .then((infuraBlockNumber) => {
+            debug(`Infura Block: ${infuraBlockNumber}`)
+            b.infuraBlockNumber = infuraBlockNumber
+            b.infuraDrift = Math.abs(b.infuraBlockNumber - b.bestBlockNumber)
+            debug(`Found Drift: ${b.infuraDrift}`)
+            // db.on('error', console.error.bind(console, 'connection error:'))
+            b.save().then((ethpeer) => {
+              debug('Saved peer: ' + ethpeer.enode)
             })
-            .catch(function (err) {
-              console.log(err)
-            })
-        }
-      })
-      .catch(function (err) {
-        console.log(err)
-      })
-  })
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+    })
 })
 
 rlpx.on('peer:removed', (peer) => {
@@ -149,6 +119,7 @@ rlpx.on('peer:removed', (peer) => {
 })
 
 for (let bootnode of BOOTNODES) {
+  debug(`Connecting to ${bootnode.address}`)
   dpt.bootstrap(bootnode).catch((err) => console.error(chalk.bold.red(err.stack || err)))
 }
 
